daf403123f3e610779346363f7abec28acfd80ab
Katherine Hambley <superdesigngirl@mac.com>
2016-02-21 09:02:12 -0600

    Initial commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KHAMBLEYMAPDEMO.Startup))]
+namespace KHAMBLEYMAPDEMO
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
